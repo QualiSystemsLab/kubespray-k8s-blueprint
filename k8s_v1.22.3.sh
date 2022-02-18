bfc6d9b2fb97bcdb26400374efdf7234fc767932
ophir.h <ophir.h@quali.com>
2022-02-18 15:09:33 +0200

    make kubespray folder env flexible

--- k8s_v1.22.3.sh
+++ k8s_v1.22.3.sh
@@ -5,7 +5,7 @@ set -e # enable exit on first error
 
 YQ_VERSION=4.15.1
 KUBESPRAY_REF="https://github.com/kubernetes-sigs/kubespray/archive/refs/tags/v2.17.1.zip"
-KUBESPRAY_FOLDER="./kubespray-2.17.1"
+KUBESPRAY_FOLDER="./kubespray*"
 
 
 # validate that all needed variables were passed from orch script
