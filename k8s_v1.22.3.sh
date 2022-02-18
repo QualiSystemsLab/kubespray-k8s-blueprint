9fcdbeddd5f8bd0195587a45f30daa91fba0358c
ophir.h <ophir.h@quali.com>
2022-02-18 13:46:55 +0200

    move kubespray ref var to the top

--- k8s_v1.22.3.sh
+++ k8s_v1.22.3.sh
@@ -4,6 +4,8 @@ set -x # enable trace
 set -e # enable exit on first error
 
 YQ_VERSION=4.15.1
+KUBESPRAY_REF="https://github.com/kubernetes-sigs/kubespray/archive/refs/tags/v2.17.1.zip"
+
 
 # validate that all needed variables were passed from orch script
 while read -r env_var; do
@@ -27,7 +29,6 @@ yum install  -y wget sshpass python3
 
 # get kubespray release version if not exist already
 KUBESPRAY_FOLDER=./kubespray
-KUBESPRAY_REF="https://github.com/kubernetes-sigs/kubespray/archive/refs/tags/v2.17.1.zip"
 if [[ ! -d "${KUBESPRAY_FOLDER}" ]]; then
         wget "${KUBESPRAY_REF}" -O ./kubespray.zip
         unzip ./kubespray.zip
