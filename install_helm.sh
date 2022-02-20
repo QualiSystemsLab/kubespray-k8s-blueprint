8812886e5d37e6f5855deb5b559e8d6eebfbd84e
Ophir Haramaty <dreckguy@gmail.com>
2022-02-20 16:15:38 +0200

    add a flexible configuration management script (#1)
    * create configuration script for K8s v1.22.3
    
    for now it;s just a copy of the original
    
    * put the specific v1.22.3 commit
    
    * fix wget line
    
    * revert to Sasha state
    
    * set KUBERNETES_VERSION
    
    * revert
    
    * silent yum
    
    * revert some
    
    * full revert
    
    * one line for yum packages
    
    * remove kubernetes version var
    
    * fix remove parsing
    
    * fix remove kubernetes version var from validation
    
    * change kubespray ref to the specific commit working for v1.22.3
    
    * revert
    
    * remove kubespray version local env
    
    * remove just kubespray version local var
    
    * remove some quotes
    
    * define kubespray ref var
    
    * move kubespray ref var to the top
    
    * add kubespray folder env again
    
    * make kubespray folder env flexible
    
    * don't use kubespray folder env
    
    * remove condition and find kubespray folder name
    
    * fix find cmd
    
    * use regex to find folder name
    
    * change kubespray ref to the right version!
    
    * fix regex
    
    * add helm script
    
    * add helm apache release
    
    * revert to Sasha kubespray ref
    
    * try fix regex
    
    * revert
    
    * revert
    
    * full revert
    
    * remove kubespray version var
    
    * set to right broken state before reverts
    
    * infer kubespray folder
    
    * fix folder handling order
    
    * remove kubernetes version handling
    
    * remove kubernetes version var
    
    * define kubespray ref local var
    
    * define ref as input param
    
    * create flexible script
    
    Co-authored-by: ophir.h <ophir.h@quali.com>

---
+++ install_helm.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+wget https://get.helm.sh/helm-v3.6.0-linux-amd64.tar.gz
+tar xvf helm-v3.6.0-linux-amd64.tar.gz 
+sudo mv linux-amd64/helm /usr/local/bin
+rm helm-v3.6.0-linux-amd64.tar.gz 
+rm -rf linux-amd64
+helm version --short
+
+helm repo add bitnami https://charts.bitnami.com/bitnami
+helm install my-release bitnami/apache
+sleep 1
+kubectl get svc
